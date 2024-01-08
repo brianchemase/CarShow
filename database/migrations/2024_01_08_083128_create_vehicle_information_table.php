b622ad2f482519ab6dfe23833b8b79ed345246b0
Elvis Makambi <makambielvis002@gmail.com>
2024-01-08 11:43:36 +0300

    vehicle information migration

---
+++ database/migrations/2024_01_08_083128_create_vehicle_information_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicle_information', function (Blueprint $table) {
+            $table->id();
+            $table->string('manufaturer');
+            $table->string('model');
+            $table->smallInteger('engine_capacity');
+            $table->string('fuel_type');
+            $table->string('transmission');
+            $table->string('body_style');
+            $table->string('seats');
+            $table->integer('mileage');
+            $table->boolean('availability');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicle_information');
+    }
+};
