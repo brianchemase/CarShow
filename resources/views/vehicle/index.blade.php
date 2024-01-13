468036e8fa4981f803f0a4b37e0ebb3fbfe2dbe1
brianchemase <brianchemo@gmail.com>
2024-01-13 17:57:05 +0300

    updated files

--- resources/views/vehicle/index.blade.php
+++ resources/views/vehicle/index.blade.php
@@ -5,18 +5,70 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- {{asset('dashboard/')}} -->
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/responsive.css')}}">
+    <title>Search Results</title>
   </head>
   <body>
-    <div class="container">
+
+
+    
+     <div class="container">
+        
+
+
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="mb-4">Filtered Vehicles</h1>    
+            </div>
+
+            <div class="row">
+            <div class="col-md-12">
+                <form>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                                    <div class="single-model-search">
+                                            <h2>Transmission</h2>
+                                            <div class="model-select-icon">
+                                                <select class="form-control" name="transmission">
+													<option disabled selected>Pick Transmission</option><!-- /.option-->
+                                                    <option value="automatic">Automatic</option><!-- /.option-->
+                                                    <option value="manual">Manual</option><!-- /.option-->
+                                                    <option value="hybrid">Hybrid</option><!-- /.option-->
+                                                    
+                                                </select><!-- /.select-->
+                                            </div><!-- /.model-select-icon -->
+                                        </div>
+                        </div>
+                        <div class="form-group col-md-6">
+                        <label for="inputPassword4">Password</label>
+                        <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                        </div>
+                    </div>
+                 
+                
+                    <button type="submit" class="btn btn-primary">Search</button>
+                </form>
+            </div>
+            </div>
+
+        </div>
+        <br>
+
+
         <div class="row">
             <div class="col-md-12">
                 <h1 class="mb-4">Filtered Vehicles</h1>
             </div>
         </div>
+
+        
+
         <div class="row">
           
                 <div class="col-md-4">
@@ -53,7 +105,7 @@
 
                 <div class="col-md-4">
                     <div class="card mb-4">
-                        <img src="{{ asset('holders/image.jpg') }}" class="card-img-top" alt="Vehicle Image Placeholder">
+                        <img src="{{ asset('holders/image1.jpg') }}" class="card-img-top" alt="Vehicle Image Placeholder">
                         <div class="card-body">
                             <h5 class="card-title">Vehicle Make Model Placeholder</h5>
                             <p class="card-text">
@@ -84,7 +136,7 @@
 
                 <div class="col-md-4">
                     <div class="card mb-4">
-                        <img src="{{ asset('holders/image.jpg') }}" class="card-img-top" alt="Vehicle Image Placeholder">
+                        <img src="{{ asset('holders/image2.jpg') }}" class="card-img-top" alt="Vehicle Image Placeholder">
                         <div class="card-body">
                             <h5 class="card-title">Vehicle Make Model Placeholder</h5>
                             <p class="card-text">
